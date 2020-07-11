864376c09dccbc93ef0f7af0fae18e2f4d401ef0
Vishav Gupta <vishavgupta2809@gmail.com>
2020-07-11 04:20:30 -0700

    Bootcamp front end

---
+++ src/ImageUpload.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const DefaultImage = null;
+class ImageUpload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      multerImage: DefaultImage,
+      imageArray: [],
+      description: "",
+      title: "",
+      url: null,
+    };
+  }
+  setDefaultImage(uploadType) {
+    if (uploadType == "multer") {
+      this.setState({
+        multerImage: DefaultImage,
+      });
+    }
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:8000/api/images";
+    let newArray = [];
+    axios.get(url).then((response) => {
+      this.setState({
+        imageArray: response.data.images,
+      });
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  handleFileChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.files[0],
+    });
+  };
+
+  uploadImage = (e) => {
+    let imageObj = {};
+
+    let imageFormObj = new FormData();
+
+    imageFormObj.append("imageName", "multer-image-" + Date.now() + ".png");
+    imageFormObj.append("imageData", this.state.multerImage);
+    imageFormObj.append("description", this.state.description);
+    imageFormObj.append("title", this.state.title);
+
+    this.setState({
+      url: URL.createObjectURL(this.state.multerImage),
+    });
+    axios
+      .post("http://localhost:8000/api/images/uploadImage", imageFormObj)
+      .then((data) => {
+        if (data.data.success) {
+          alert("Image has been successfully uploaded using multer");
+          this.setDefaultImage("multer");
+        }
+      })
+      .catch((err) => {
+        alert("Error uploading the image");
+        this.setDefaultImage("multer");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="process">
+          <h4>Process:Uploading Image</h4>
+          <input
+            type="file"
+            className="process_upload-btn"
+            onChange={this.handleFileChange}
+            id="multerImage"
+          />
+          <div>
+            <input
+              type="text"
+              placeholder="description of project"
+              id="description"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Title of the project"
+              id="title"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+
+          <button onClick={this.uploadImage}>Upload</button>
+        </div>
+        {this.state.imageArray.length > 0
+          ? this.state.imageArray.map((image, index) => (
+              <div key={index}>
+                <h1>{image.title}</h1>
+                <img
+                  src={`http://localhost:8000/uploads/${image.imageData}`}
+                  alt="Uploaded image"
+                  height="300"
+                  width="400"
+                  key={index}
+                  style={{
+                    height: 400,
+                    width: 500,
+                    display: "block",
+                    margin: 20,
+                    padding: 10,
+                    border: 1,
+                    borderColor: "black",
+                    margin: "auto",
+                  }}
+                />
+                <h3>{image.description}</h3>
+              </div>
+            ))
+          : null}
+      </div>
+    );
+  }
+}
+
+export default ImageUpload;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkqU2W03zb6OZBtqtf803cNBg-ohxakxI",
+  authDomain: "bootcamp-8265e.firebaseapp.com",
+  databaseURL: "https://bootcamp-8265e.firebaseio.com",
+  projectId: "bootcamp-8265e",
+  storageBucket: "bootcamp-8265e.appspot.com",
+  messagingSenderId: "240091523370",
+  appId: "1:240091523370:web:19aebde67f701912b95653",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default storage;
